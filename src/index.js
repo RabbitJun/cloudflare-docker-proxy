2dd368caf25bd5844061d6769eeeb333b3bd60f1
RabbitJun <68361022+RabbitJun@users.noreply.github.com>
2024-06-16 17:20:39 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.rbtstudio.cc": "https://registry-1.docker.io",
+  "quay.rbtstudio.cc": "https://quay.io",
+  "gcr.rbtstudio.cc": "https://gcr.io",
+  "k8s-gcr.rbtstudio.cc": "https://k8s.gcr.io",
+  "k8s.rbtstudio.cc": "https://registry.k8s.io",
+  "ghcr.rbtstudio.cc": "https://ghcr.io",
+  "cloudsmith.rbtstudio.cc": "https://docker.cloudsmith.io",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
